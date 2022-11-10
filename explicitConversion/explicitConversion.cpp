5af490431813e6db439a8c9a87c1e41b7f2c5f26
User <User@DESKTOP-81L93II>
2022-11-10 21:26:50 +0300

    Add project files.

---
+++ explicitConversion/explicitConversion.cpp
@@ -0,0 +1,17 @@
+// explicitConversion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ //variable declaration and intializaiton
+	float mAge = 10.123;
+
+	//explicit conversion
+	int ageM = int(mAge);
+
+		cout << ageM;
+}
+
